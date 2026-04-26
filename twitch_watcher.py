2d4720488f2b2b2675473b7490210468e18c1554
zfabro <fabriparente02@gmail.com>
2026-04-26 20:59:55 -0300

    Update twitch_watcher.py

--- twitch_watcher.py
+++ twitch_watcher.py
@@ -27,7 +27,7 @@ def log(mensaje):
 
 def obtener_headers():
     return {
-        "Authorization": f"OAuth {AUTH_TOKEN}",
+        "Authorization": f"Bearer {AUTH_TOKEN}",
         "Client-Id": CLIENT_ID,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         "Content-Type": "application/json",
